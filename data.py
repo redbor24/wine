94f6846290de2f8d8a955fbf48a3c59445c638b6
redbor <redbor24@gmail.com>
2021-12-01 18:03:27 +0300

    Считывание wine2.xlsx и вывод через pprint

--- data.py
+++ data.py
@@ -1,6 +1,19 @@
 import pandas
+from pprint import pprint
 
 
 def load_excel(filename):
     excel_data_df = pandas.read_excel(filename, sheet_name='Лист1')
     return excel_data_df.to_dict(orient='records')
+
+
+if __name__ == '__main__':
+    excel_data = pandas.read_excel(
+        'wine2.xlsx',
+        sheet_name='Лист1',
+        na_values='nan',
+        keep_default_na=False
+    )
+    excel_dict = excel_data.to_dict(orient='records')
+    # print(excel_dict)
+    print(pprint(excel_dict))
