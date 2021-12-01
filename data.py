f45c9a91c7c0695a45c4894b3e5c7200d7869b1b
redbor <redbor24@gmail.com>
2021-12-01 17:20:28 +0300

    Карточки вин генерируются в цикле на основании данных из data.wines

---
+++ data.py
@@ -0,0 +1,15 @@
+wines = [
+    {
+        'title': 'Изабелла',
+        'grape': 'Изабелла',
+        'price': 350,
+        'img_link': 'images/izabella.png'
+    },
+    {
+        'title': 'Гранатовый браслет',
+        'grape': 'Мускат розовый',
+        'price': 351,
+        'img_link': 'images/granatovyi_braslet.png'
+    },
+
+]
--- main.py
+++ main.py
@@ -4,19 +4,8 @@ from http.server import HTTPServer, SimpleHTTPRequestHandler
 
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 
-
-def plural_days(n):
-    days = ['год', 'года', 'лет']
-
-    if n % 10 == 1 and n % 100 != 11:
-        p = 0
-    elif 2 <= n % 10 <= 4 and (n % 100 < 10 or n % 100 >= 20):
-        p = 1
-    else:
-        p = 2
-
-    return str(n) + ' ' + days[p]
-
+import tools
+import data
 
 env = Environment(
     loader=FileSystemLoader('.'),
@@ -29,7 +18,8 @@ company_old = relativedelta(datetime.today(), company_founded).years
 template = env.get_template('template.html')
 
 rendered_page = template.render(
-    company_old=plural_days(company_old)
+    company_old=tools.plural_days(company_old),
+    wines=data.wines
 )
 
 with open('index.html', 'w', encoding="utf8") as file:
