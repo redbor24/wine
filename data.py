3b94622bdda01661abaafc80d3adb7ea827892b2
redbor <redbor24@gmail.com>
2021-12-01 18:50:24 +0300

    Преобразование данных из файла wine2.xlsx выполняется посредством collections.defaultdict()

--- data.py
+++ data.py
@@ -1,3 +1,4 @@
+import collections
 import pandas
 from pprint import pprint
 
@@ -18,7 +19,7 @@ def load_excel2(filename):
 
 
 def get_restructed_excel_data(excel_data):
-    restructed_excel_data = {}
+    restructed_excel_data = collections.defaultdict(list)
 
     for item_n, item in enumerate(excel_data):
         wine = {
@@ -27,15 +28,20 @@ def get_restructed_excel_data(excel_data):
             'Сорт': item['Сорт'],
             'Цена': item['Цена'],
         }
-        if restructed_excel_data.get(item['Категория']):
-            restructed_excel_data[item['Категория']].append(wine)
-        else:
-            restructed_excel_data[item['Категория']] = [wine]
+        restructed_excel_data[item['Категория']].append(wine)
 
     return restructed_excel_data
 
 
 if __name__ == '__main__':
+    # dict_of_lists = collections.defaultdict(list)
+    # print(dict_of_lists)
+    # dict_of_lists['a'].append({'a': 'a', 'x': 1})
+    # dict_of_lists['a'].append({'a': 'a2', 'x': 2})
+    # dict_of_lists['b'].append({'b': 'b1', 'x': 3})
+    # pprint(dict_of_lists)
+    # exit()
+
     excel_dict = load_excel2('wine2.xlsx')
-    # print(pprint(excel_dict))
-    print(pprint(get_restructed_excel_data(excel_dict)))
+    pprint(get_restructed_excel_data(excel_dict))
+
