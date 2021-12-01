5bb3853ffcca33215be69e553991dbdcb09e2132
redbor <redbor24@gmail.com>
2021-12-01 18:30:15 +0300

    Данные файла wine2.xlsx преобразованы в требуемый на 12 шаге формат.

--- data.py
+++ data.py
@@ -7,13 +7,35 @@ def load_excel(filename):
     return excel_data_df.to_dict(orient='records')
 
 
-if __name__ == '__main__':
+def load_excel2(filename):
     excel_data = pandas.read_excel(
-        'wine2.xlsx',
+        filename,
         sheet_name='Лист1',
         na_values='nan',
         keep_default_na=False
     )
-    excel_dict = excel_data.to_dict(orient='records')
-    # print(excel_dict)
-    print(pprint(excel_dict))
+    return excel_data.to_dict(orient='records')
+
+
+def get_restructed_excel_data(excel_data):
+    restructed_excel_data = {}
+
+    for item_n, item in enumerate(excel_data):
+        wine = {
+            'Картинка': item['Картинка'],
+            'Название': item['Название'],
+            'Сорт': item['Сорт'],
+            'Цена': item['Цена'],
+        }
+        if restructed_excel_data.get(item['Категория']):
+            restructed_excel_data[item['Категория']].append(wine)
+        else:
+            restructed_excel_data[item['Категория']] = [wine]
+
+    return restructed_excel_data
+
+
+if __name__ == '__main__':
+    excel_dict = load_excel2('wine2.xlsx')
+    # print(pprint(excel_dict))
+    print(pprint(get_restructed_excel_data(excel_dict)))
