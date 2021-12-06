5fba8e123b5b64060a51e952a0d0526fb1583ef8
redbor <redbor24@gmail.com>
2021-12-06 10:23:43 +0300

    Список вин для загрузки можно задать через командную строку

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 from http.server import HTTPServer, SimpleHTTPRequestHandler
 
+import configargparse
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 
 import data
 from tools import get_company_age
+
+p = configargparse.ArgParser()
+p.add_argument('-df', required=True, type=str,
+               help='Excel-list of wines to load')
+args = p.parse_args()
+wines_excel_file = args.df
 
 env = Environment(
     loader=FileSystemLoader('.'),
@@ -14,7 +21,7 @@ template = env.get_template('template.html')
 
 rendered_page = template.render(
     company_age=get_company_age(),
-    wines=data.load_excel('wine3.xlsx')
+    wines=data.load_excel(wines_excel_file)
 )
 
 with open('index.html', 'w', encoding="utf8") as file:
