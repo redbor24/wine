ef6d8fc9e0b950557b319951c171d43ad5429790
redbor <redbor24@gmail.com>
2021-12-07 17:15:45 +0300

    Оптимизация наименований функций и переменных.

--- main.py
+++ main.py
@@ -4,14 +4,17 @@ import configargparse
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 
 import data
-from tools import get_company_age
+from tools import get_company_age_caption
 
 
 def main():
     arg_parser = configargparse.ArgParser()
-    arg_parser.add_argument('-df', required=True, type=str,
-                   help='List of wines to load in Excel format. ' 
-                        'See "wineslist_template.xlsx" for example')
+    arg_parser.add_argument(
+        '-df',
+        required=True,
+        type=str,
+        help='List of wines to load in Excel format.'
+             ' See "wineslist_template.xlsx" for example')
     args = arg_parser.parse_args()
     wines_excel_file = args.df
 
@@ -23,7 +26,7 @@ def main():
     template = env.get_template('template.html')
 
     rendered_page = template.render(
-        company_age=get_company_age(),
+        company_age_caption=get_company_age_caption(),
         wines=data.import_excel_datafile(wines_excel_file)
     )
 
--- tools.py
+++ tools.py
@@ -1,24 +1,21 @@
-from datetime import datetime
-
-from dateutil.relativedelta import relativedelta
+import time
 
 WINE_COMPANY_BIRTH_YEAR = 1920
 
 
-def get_year_plural(year):
+def get_year_plural_name(year):
     year_plural_names = ['год', 'года', 'лет']
 
     if year % 10 == 1 and year % 100 != 11:
-        p = 0
+        year_name_index = 0
     elif 2 <= year % 10 <= 4 and (year % 100 < 10 or year % 100 >= 20):
-        p = 1
+        year_name_index = 1
     else:
-        p = 2
+        year_name_index = 2
 
-    return str(year) + ' ' + year_plural_names[p]
+    return str(year) + ' ' + year_plural_names[year_name_index]
 
 
-def get_company_age():
-    return get_year_plural(relativedelta(
-        datetime.today(),
-        datetime(year=WINE_COMPANY_BIRTH_YEAR, month=1, day=1)).years)
+def get_company_age_caption():
+    current_year = int(time.strftime('%Y', time.localtime()))
+    return get_year_plural_name(current_year - WINE_COMPANY_BIRTH_YEAR)
